e775a9fa5a8079ccc278f7f5aadf93d974c6c6b6
jordan.wyberneit@gmail.com <jordan.wyberneit@gmail.com>
2025-06-30 19:56:55 +0000

    aligning ingest files with new macros for data typing

--- models/staging/bigquery/citibike-nyc/stg_citibike_202408.sql
+++ models/staging/bigquery/citibike-nyc/stg_citibike_202408.sql
@@ -1,15 +1,3 @@
-select 
-    CAST(ride_id AS STRING) as ride_id, 
-    CAST(rideable_type AS STRING) as rideable_type, 
-    CAST(started_at AS TIMESTAMP) as started_at, 
-    CAST(ended_at AS TIMESTAMP) as ended_at, 
-    CAST(start_station_name AS STRING) as start_station_name, 
-    CAST(start_station_id AS STRING) as start_station_id, 
-    CAST(`end station_name` AS STRING) as end_station_name, 
-    CAST(end_station_id AS STRING) as end_station_id, 
-    CAST(start_lat AS NUMERIC) as start_lat, 
-    CAST(start_lng AS NUMERIC) as start_lng, 
-    CAST(end_lat AS NUMERIC) as end_lat, 
-    CAST(end_lng AS NUMERIC) as end_lng, 
-    CAST(member_casual AS STRING) as member_casual
-from {{ source('citibike_nyc_2024', '202408-citibike-tripdata') }} 
+{{ config(materialized='table') }}
+
+{{ clean_citibike_staging('citibike_nyc_2024', '202408-citibike-tripdata') }}
